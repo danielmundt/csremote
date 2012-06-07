85bd36aa781b70dab8b78dbcbb6b9d1318179f2f
Daniel Schubert <d.schubert@aboutnoise.com>
2012-06-07 18:23:54 +0200

    Configure project to use .NET Framwork 2.0

--- source/Remoting.Client/FormMain.cs
+++ source/Remoting.Client/FormMain.cs
@@ -24,7 +24,6 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
-using System.Linq;
 using System.Runtime.Remoting;
 using System.Runtime.Remoting.Channels;
 using System.Runtime.Remoting.Channels.Ipc;

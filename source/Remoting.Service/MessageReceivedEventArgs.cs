85628cccbcbe6681825d6473076f68a5b256d616
Daniel Schubert <d.schubert@aboutnoise.com>
2012-06-10 12:29:03 +0200

    Add MessageReceivedEventArgs class

---
+++ source/Remoting.Service/MessageReceivedEventArgs.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Remoting.Service
+{
+    public class MessageReceivedEventArgs : EventArgs
+    {
+        private object userObject;
+
+        public MessageReceivedEventArgs(Object userObject)
+        {
+            this.userObject = userObject;
+        }
+
+        public Object UserObject
+        {
+            get
+            {
+                return userObject;
+            }
+        }
+    }
+}
